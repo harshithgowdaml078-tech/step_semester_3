8f010bdf44168be6c21c0809f62e3c2f715479e5
harshithgowdaml078-tech <harshithgowdaml078@gmail.com>
2026-09-11 23:58:48 +0530

    Session 5: Arrays and Methods class and assignment problems

---
+++ src/week5/practice/TopPerformerTracker.java
@@ -0,0 +1,16 @@
+package week5.practice;
+
+public class TopPerformerTracker {
+    static String findMinMaxSpread(int[] scores) {
+        int min = scores[0], max = scores[0];
+        for (int score : scores) {
+            if (score < min) min = score;
+            if (score > max) max = score;
+        }
+        return "Min: " + min + " | Max: " + max + " | Spread: " + (max - min);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMinMaxSpread(new int[]{45, 82, 79, 90, 33, 90, 61}));
+    }
+}
